839dab9fdadf59dae162a274cd5e52299e701a17
corvuscorae <corvuscorae@gmail.com>
2024-11-07 09:14:32 -0800

    tuned z3 logic and randomness

--- z3demo.js
+++ z3demo.js
@@ -2,6 +2,7 @@ import { init } from 'z3-solver';
 
 const { Context } = await init();
 const { Solver, Int, And, Or, Distinct } = new Context("main");
+const debug = true; // toggle testing
 
 /*********************************************************************************
 /   CHILDREN AND PETS PUZZLE
@@ -56,14 +57,19 @@ const sue_val = puzzle_model.eval(sue);
 console.log(` > bob: ${bob_val}\n > mary: ${mary_val}\n > cathy: ${cathy_val}\n > sue: ${sue_val}`);
 
 // testing
-const expectedOutput =  [ `2`,  `1`,   `4`, `3` ]; 
-const actualOutput = [`${bob_val}`, `${mary_val}`, `${cathy_val}`, `${sue_val}`];
-console.log(checkOutput(expectedOutput, actualOutput) ? "PASSED\n" : "FAILED\n");
+if(debug){
+    const expectedOutput =  [ `2`,  `1`,   `4`, `3` ]; 
+    const actualOutput = [`${bob_val}`, `${mary_val}`, `${cathy_val}`, `${sue_val}`];
+    console.log(checkOutput(expectedOutput, actualOutput) ? "PASSED\n" : "FAILED\n");
+}
 
 /*********************************************************************************
 /   REPRESENT GAME CONSTRAINTS
 /********************************************************************************/
 
+//** HELPER FCNS **/
+// randomRange()
+//  > generate a random range from a given [min, max] pair
 function randomRange(min, max, mult){    // min and max are inclusive
     if(min == null){ 
         min = Math.ceil(Math.random() * mult); 
@@ -90,58 +96,76 @@ function randomRange(min, max, mult){    // min and max are inclusive
     else{ return [b, a]; }
 };
 
+// randomInRange()
+//  > pick a random in [min, max]
 function randomInRange(min, max){    // min and max are inclusive
     let [min_ceil, max_ceil] = [Math.ceil(min), Math.ceil(max)];
     
     return Math.floor(Math.random() * (max_ceil - min_ceil + 1) + min_ceil);
 };
 
-function checkOutputRange([x, y], [l, r], [t, b], enforceBounds){
-    if(x < l || x > r || y < t || y > b){ return false; }
+// pickSide()
+//  > given a value, pick the "side" it's closer to (min or max)
+function pickSide(value, min, max){
+    let mid = (min + max) / 2;
+    if(value > mid){ return max; }
+    else{ return min; }
+}
 
-    if(enforceBounds){ // check that x or y is equal to a boundary
-        if(x != l && x != r && y != t && y != b){ 
-            return false; 
-        }
+// checkOutputRange()
+//  > debug: make sure that sat model gives expected values within a range
+function checkOutputRange([x, y], [l, r], [t, b], enforce_mode){
+    switch(enforce_mode){
+        case "outside":
+            if(x > l && x < r && y > t && y < b){ return false; }
+            break;
+        case "boundary": // check that x or y is equal to a boundary
+            if(x != l && x != r && y != t && y != b){ return false; }
+        default:
+            if(x < l || x > r || y < t || y > b){ return false; }
+        break;
     }
+    // IF X IS WITHIN OG RANGE, THEN Y HAS TO BE OUTSIDE
+    // IF Y IS WITHIN OG RANGE, THEN X HAS TO BE OUTSIDE
+
 
     return true;
 };
 
+/** VARIABLES **/
+// constraints:
 // LEFT fence side is at tile   x = 5
 // RIGHT fence side is at       x = 10
 // TOP fence side is at         y = 15
 // BOTTOM fence side is at      y = 25
-
 const [left, right] = [5, 10];
 const [top, bottom] = [15, 25];
 
 const [x, y] = [Int.const("x"), Int.const("y")];
 
-const MULTIPLIER = 100;
+const MULTIPLIER = 100; // used by randomRange when min/max is null
 
 /***** GENERATE INSIDE FENCE ****************************************************/
 console.log("GENERATE INSIDE FENCE");
 
 const inside_fence_solver = new Solver();
 
-let [x_min, x_max] = randomRange(left, right);
-let [y_min, y_max] = randomRange(top, bottom);
+let [x_min, x_max] = randomRange(left, right);  // generate new range based on original range
+let [y_min, y_max] = randomRange(top, bottom);  //      to increase randomness
+
+// another option for randomness: 
+// let x_rand = randomInRange(left+1, right-1);
+// let y_rand = randomInRange(top+1, bottom-1);
+// (or is this essentially just doing z3's work for it?)
 
 inside_fence_solver.add( 
     And(
-        x.ge(x_min), 
-        x.le(x_max),
-        x.neq(left),
-        x.neq(right)
-    ) 
-);
-inside_fence_solver.add( 
+        x.ge(x_min), x.le(x_max),   // x is in this range
+        x.neq(left), x.neq(right)   // ...such that x is not on either border
+    ),
     And(
-        y.ge(y_min), 
-        y.le(y_max),
-        y.neq(top),
-        y.neq(bottom)
+        y.ge(y_min), y.le(y_max),   // y is in this range
+        y.neq(top), y.neq(bottom)   // ...such that y is not on either border
     ) 
 );
 
@@ -156,31 +180,34 @@ let y_val = inside_fence_model.eval(y);
 console.log(` > (x, y): (${x_val}, ${y_val})`);
 
 //test
-console.log(
-    checkOutputRange(
-        [`${x_val}`, `${y_val}`], 
-        [left, right], 
-        [top, bottom]
-    ) ? "PASSED\n" : "FAILED\n"
-);
+if(debug){
+    console.log(
+        checkOutputRange(
+            [`${x_val}`, `${y_val}`], 
+            [left, right], 
+            [top, bottom]
+        ) ? "PASSED\n" : "FAILED\n"
+    );
+}
 
 /***** GENERATE ON FENCE ********************************************************/
 console.log("GENERATE ON FENCE");
 
 const on_fence_solver = new Solver();
 
-[x_min, x_max] = randomRange(left, right);
-[y_min, y_max] = randomRange(top, bottom);
+let x_rand = randomInRange(left, right);    // again, generating a new range
+let y_rand = randomInRange(top, bottom);    //  to give a more random feel
 
 on_fence_solver.add( 
+    x.eq(x_rand),
     Or(
-        And(
-            Or(x.eq(left), x.eq(right)),
-            And(y.ge(y_min), y.le(y_max))
+        And(    
+            Or(x.eq(left), x.eq(right)),    // x is on one of its borders
+            y.eq(y_rand)                    // y is random
         ),
         And(
-            Or(y.eq(top), y.eq(bottom)),
-            And(x.ge(x_min), x.le(x_max))
+            And(x.neq(left), x.neq(right)),         // x isn't on a border
+            y.eq(pickSide(y_rand, top, bottom))     // y is on one of its borders
         )
     )
 );
@@ -196,14 +223,16 @@ y_val = on_fence_model.eval(y);
 console.log(` > (x, y): (${x_val}, ${y_val})`);
 
 //test
-console.log(
-    checkOutputRange(
-        [`${x_val}`, `${y_val}`], 
-        [left, right], 
-        [top, bottom],
-        true
-    ) ? "PASSED\n" : "FAILED\n"
-);
+if(debug){
+    console.log(
+        checkOutputRange(
+            [`${x_val}`, `${y_val}`], 
+            [left, right], 
+            [top, bottom],
+            "boundary"
+        ) ? "PASSED\n" : "FAILED\n"
+    );
+}
 
 /***** GENERATE OUTSIDE FENCE ***************************************************/
 console.log("GENERATE OUTSIDE FENCE");
@@ -213,26 +242,25 @@ const outside_fence_solver = new Solver();
 let outside_x_min = 8;  // must be at a x value of 8 or greater...
 let outside_y_min = 20; // ...and a y value of 20 or greater
 
+// this time, our randomm-feeling ranges are generated without maxes
+//      and mins are based on constraints.
 [x_min, x_max] = randomRange(outside_x_min, null, MULTIPLIER); 
 [y_min, y_max] = randomRange(outside_y_min, null, MULTIPLIER); 
 
 outside_fence_solver.add( 
     And(
-        x.ge(x_min), 
-        x.ge(outside_x_min),        
-        x.le(x_max),
-        x.neq(left),
-        x.neq(right)
-    ) 
-);
-outside_fence_solver.add( 
+        x.ge(x_min), x.le(x_max),       // x is in our random range
+        x.neq(left), x.neq(right)       // x is not on a border
+    ), 
     And(
-        y.ge(y_min),
-        y.ge(outside_y_min),        
-        y.le(y_max),
-        y.neq(top),
-        y.neq(bottom)
-    ) 
+        y.ge(y_min), y.le(y_max),       // y is in our random range
+        y.neq(top), y.neq(bottom)       // y is not on a border
+    ),
+    Or(
+        And(x.lt(right), y.gt(bottom)), // x is in fence range, y is out of fence range
+        And(y.lt(bottom), x.gt(right)), // vice-versa
+        And(x.gt(right), y.gt(bottom))  // x and y are both out of fence range
+    )
 );
 
 let ouside_fence_check = outside_fence_solver.check();
@@ -246,10 +274,13 @@ y_val = outside_fence_model.eval(y);
 console.log(` > (x, y): (${x_val}, ${y_val})`);
 
 //test
-console.log(
-    checkOutputRange(
-        [`${x_val}`, `${y_val}`], 
-        [outside_x_min, Number.INFINITY], 
-        [outside_y_min, Number.INFINITY],
-    ) ? "PASSED\n" : "FAILED\n"
+if(debug){
+    console.log(
+        checkOutputRange(
+            [`${x_val}`, `${y_val}`], 
+            [left, right], 
+            [top, bottom],
+            "outside"
+        ) ? "PASSED\n" : "FAILED\n"
 );
+}
